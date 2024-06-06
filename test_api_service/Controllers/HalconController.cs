ca0a9f94f30509545f349a94bd23eace725c16e6
JirawatChayayan <51415869+JirawatChayayan@users.noreply.github.com>
2024-06-06 17:03:32 +0700

    Add project files.

---
+++ test_api_service/Controllers/HalconController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HalconDotNet;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace test_api_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HalconController : ControllerBase
+    {
+  
+
+        // GET: api/<HalconController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            xxx.xxxx += $"{DateTime.Now} Request {Environment.NewLine}";
+            try
+            {
+                HOperatorSet.GetSystem("hostids", out HTuple res);
+                string[] data = new string[res.Length];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    data[i] = res[i].S;
+                }
+                xxx.xxxx += $"{DateTime.Now} OK {Environment.NewLine}";
+                return Ok(data);
+            }
+            catch(Exception ex)
+            {
+                xxx.xxxx += $"{DateTime.Now} Fail {ex.Message} {Environment.NewLine}";
+                return BadRequest(ex.Message);
+            }
+            
+        }
+
+        [HttpGet("error")]
+        public ActionResult Get2()
+        {
+            return Ok(xxx.xxxx);
+        }
+    }
+
+}
